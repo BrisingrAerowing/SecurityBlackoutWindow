c2375b3c714ece90e26c715776b472e86e2bdcf9
Brisingr Aerowing <ztgreve@gmail.com>
2014-12-11 23:12:50 -0500

    Initial Commit

---
+++ src/SecurityBlackoutWindow/Blackout.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading;
+
+namespace SecurityBlackoutWindow
+{
+    public static class BlackoutWindow
+    {
+
+        public static void Blackout(IntPtr hWnd, Action action)
+        {
+            Blackout(hWnd, () =>
+            {
+                action();
+                return (object)null;
+            });
+        }
+
+        public static T Blackout<T>(IntPtr hParentWnd, Func<T> func)
+        {
+            NativeMethods.WNDCLASSEX wndClass = new NativeMethods.WNDCLASSEX();
+            
+            wndClass.cbSize = (UInt32)Marshal.SizeOf<NativeMethods.WNDCLASSEX>();
+            wndClass.style = 0;
+            wndClass.lpfnWndProc = NativeMethods.DefWindowProc;
+            wndClass.cbClsExtra = 0;
+            wndClass.cbWinExtra = 0;
+            wndClass.hInstance = Process.GetCurrentProcess().Handle;
+            wndClass.hIcon = IntPtr.Zero;
+            wndClass.hIconSm = IntPtr.Zero;
+            wndClass.hbrBackground = NativeMethods.GetSysColorBrush(NativeMethods.COLOR_DESKTOP);
+            wndClass.lpszMenuName = null;
+            wndClass.lpszClassName = "Blackout";
+
+            IntPtr hWndClassEx = Marshal.AllocHGlobal((int)wndClass.cbSize);
+            Marshal.StructureToPtr(wndClass, hWndClassEx, false);
+            
+            IntPtr hStopWnd;
+            NativeMethods.RECT rectParent;
+            
+            if(NativeMethods.RegisterClassEx(hWndClassEx) == IntPtr.Zero)
+            {
+                throw new Win32Exception();
+            }
+
+            if (hParentWnd == IntPtr.Zero) hParentWnd = NativeMethods.GetDesktopWindow();
+
+            NativeMethods.GetWindowRect(hParentWnd, out rectParent);
+
+            int nWidth = rectParent.right - rectParent.left;
+            int nHeight = rectParent.bottom - rectParent.top;
+
+            hStopWnd = NativeMethods.CreateWindowEx(NativeMethods.WS_EX_LAYERED,
+                                                    wndClass.lpszClassName,
+                                                    null,
+                                                    NativeMethods.WS_VISIBLE | NativeMethods.WS_POPUP,
+                                                    rectParent.left,
+                                                    rectParent.top,
+                                                    nWidth,
+                                                    nHeight,
+                                                    hParentWnd,
+                                                    IntPtr.Zero,
+                                                    wndClass.hInstance,
+                                                    IntPtr.Zero);
+
+            if (hStopWnd == IntPtr.Zero) throw new Win32Exception();
+
+            NativeMethods.SetLayeredWindowAttributes(hStopWnd, 0, 255, NativeMethods.LWA_ALPHA);
+
+            Thread.Sleep(500);
+
+            NativeMethods.SetLayeredWindowAttributes(hStopWnd, 0, 196, NativeMethods.LWA_ALPHA);
+
+            var res = func();
+
+            NativeMethods.DestroyWindow(hStopWnd);
+
+            NativeMethods.UnregisterClass(wndClass.lpszClassName, wndClass.hInstance);
+
+            try
+            {
+                Marshal.FreeHGlobal(hWndClassEx);
+            }
+            catch
+            {
+
+            }
+
+            return res;
+            
+        }
+
+    }
+}
---
+++ src/SecurityBlackoutWindow/NativeMethods.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SecurityBlackoutWindow
+{
+    internal static class NativeMethods
+    {
+
+        internal const Int32 LWA_ALPHA = 0x00000002;
+        internal const UInt32 WS_POPUP = 0x80000000;
+        internal const UInt32 WS_VISIBLE = 0x10000000;
+        internal const UInt32 WS_EX_LAYERED = 0x00080000;
+        internal const Int32 COLOR_DESKTOP = 1;
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        internal delegate IntPtr WNDPROC(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WNDCLASSEX
+        {
+            public UInt32 cbSize;
+            public UInt32 style;
+            [MarshalAs(UnmanagedType.FunctionPtr)]
+            public WNDPROC lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWinExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            public string lpszMenuName;
+            public string lpszClassName;
+            public IntPtr hIconSm;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct RECT
+        {
+            public Int32 left;
+            public Int32 top;
+            public Int32 right;
+            public Int32 bottom;
+        }
+
+        [DllImport("User32.dll")]
+        internal static extern IntPtr CreateWindowEx(UInt32 dwExStyle, string lpClassName, string lpWindowName, UInt32 dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+
+        [DllImport("User32.dll")]
+        internal static extern bool DestroyWindow(IntPtr hWnd);
+
+        [DllImport("User32.dll")]
+        internal static extern bool SetLayeredWindowAttributes(IntPtr hWnd, UInt32 crKey, byte bAlpha, int dwFlags);
+
+        [DllImport("User32.dll")]
+        internal static extern IntPtr DefWindowProc(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        internal static extern IntPtr RegisterClassEx(IntPtr wndClassEx);
+
+        [DllImport("User32.dll")]
+        internal static extern IntPtr GetDesktopWindow();
+
+        [DllImport("User32.dll")]
+        internal static extern IntPtr GetSysColorBrush(Int32 nIndex);
+
+        [DllImport("User32.dll")]
+        internal static extern bool GetWindowRect(IntPtr hWnd, [Out]out RECT lpRect);
+
+        [DllImport("User32.dll")]
+        internal static extern bool UnregisterClass(string lpClassName, IntPtr hInstance);
+
+
+
+    }
+}
